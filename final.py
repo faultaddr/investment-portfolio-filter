bf4367c3db64d2b9b6e5b52f3df71e2dfc336fee
David-AJ <812377857@qq.com>
2017-09-18 11:09:53 +0800

    修改数据

--- final.py
+++ final.py
@@ -4,6 +4,7 @@ from investment import bag
 from similarity import *
 import scipy.spatial.distance as spd
 import numpy as np
+import pandas as pd
 
 def to_old():
     d
--- similarity.py
+++ similarity.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 #-*-coding=utf-8-*-
-import scipy.spatial.distance as spd
+from scipy.spatial import distance as spd
 import numpy as np
 
 # 老用户协同过滤
 # 输入交易记录总表，K
 # 输出每个用户前K个相似用户
-def filter_o(history,K):
-    temp = spd.pdist(history,'cosine')
-    temp[range(len(temp)),range(len(temp))] = 0
-    re = np.argsort(temp)
-    return np.array(re[:,0:K])
+def filter_o(id,history,K):
+    temp = 1 - spd.cdist(np.array([history[id]]),history,'cosine') # cdist is dissimilarity
+    temp[0][id] = 0
+    re = np.argsort(temp[0])
+    return re[0,:K]
 
 # 新用户协同过滤
 # 输入一个新用户属性，老用户全部属性，K
 # 输出新用户前K个相似用户
 def filter_n(new,users,K):
-    temp = spd.cdist(new,users,'cosine')
-    temp[0] = 0
-    re = np.argsort(temp)
-    return np.array(re[:,0:K])
+    temp = 1 - spd.cdist(np.array([new]),users,'cosine') # cdist is dissimilarity
+    re = np.argsort(temp[0])
+    return re[0,:K]
