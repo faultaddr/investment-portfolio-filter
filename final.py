af7fde6b20ab9a55aef10602bba9751278289970
David-AJ <812377857@qq.com>
2017-09-20 19:04:01 +0800

    用户画像&协同过滤 TODO:背包

--- final.py
+++ final.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-from investment import bag
 from similarity import *
-import scipy.spatial.distance as spd
 import numpy as np
-import pandas as pd
+import sys
 
-def to_old():
-    d
+history = pd.read_csv(u'交易数据.csv')
+history = history.as_matrix()
+users = pd.read_csv(u'用户属性.csv')
+us = users.as_matrix()
+production = pd.read_csv(u'产品.csv')
+production = production.as_matrix()
 
-def to_new():
-    d
+if __name__ =='__main__':
+    money = int(sys.argv[2])
+    if sys.argv[1]=='new':
+        new = np.loadtxt(u'new_u.txt')
+        [index, Group] = to_new(new,money,us,history,production)
+        with open(u'new.txt','w') as f:
+            for i in index:
+                f.write(str(i)+'\n')
+    else:
+        u_id = sys.argv[3]
+        [index, Group] = to_old(u_id,money,history,production)
+        with open(u'old.txt','w') as f:
+            for i in index:
+                f.write(str(i)+'\n')
+    picture(Group, users)
